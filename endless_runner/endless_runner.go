d29b697c4d13f9f587a5ed878a3b433dd6839ad2
David Banham <david@banham.id.au>
2015-09-20 11:59:34 +1000

    gofmt

--- endless_runner/endless_runner.go
+++ endless_runner/endless_runner.go
@@ -1,60 +1,60 @@
 package endless_runner
 
 import (
-  "os/exec"
-  "os"
+	"os"
+	"os/exec"
 )
 
 func Run(name string, args ...string) (chan *os.Process, chan error, chan bool) {
 
-  //Make a communication channel that transmits processes
-  commChannel := make(chan *os.Process)
+	//Make a communication channel that transmits processes
+	commChannel := make(chan *os.Process)
 
-  // Make a channel to pass back errors on
-  errChannel := make(chan error)
+	// Make a channel to pass back errors on
+	errChannel := make(chan error)
 
-  // Make a channel to listen for stop events
-  stopChannel := make(chan bool)
+	// Make a channel to listen for stop events
+	stopChannel := make(chan bool)
 
-  go waiter(commChannel, errChannel, stopChannel, name, args...)
+	go waiter(commChannel, errChannel, stopChannel, name, args...)
 
-  return commChannel, errChannel, stopChannel
+	return commChannel, errChannel, stopChannel
 }
 
 func runProc(name string, args ...string) (*exec.Cmd, error) {
-  cmd := exec.Command(name, args...)
+	cmd := exec.Command(name, args...)
 
-  //cmd.Stdout = os.Stdout
-  //cmd.Stderr = os.Stderr
+	//cmd.Stdout = os.Stdout
+	//cmd.Stderr = os.Stderr
 
-  err := cmd.Start()
-  return cmd, err
+	err := cmd.Start()
+	return cmd, err
 }
 
 // Waiter takes a channel that speaks processes, a command name, and some arguments
 // It runs that command with the arguments and passes teh resulting process back across the channel
 // It them waits for that process to exit. When it does, it recurses. This causes the process to be restarted and the new process passed back along the channel
 func waiter(comm chan *os.Process, errs chan error, stops chan bool, name string, args ...string) {
-  cmd, err := runProc(name, args...)
+	cmd, err := runProc(name, args...)
 
-  shouldStop := false
+	shouldStop := false
 
-  go func () {
-    shouldStop = <- stops
-    cmd.Process.Kill()
-    cmd.Process.Release()
-    stops <- true
-  }()
+	go func() {
+		shouldStop = <-stops
+		cmd.Process.Kill()
+		cmd.Process.Release()
+		stops <- true
+	}()
 
-  if err != nil {
-    errs <- err
-    return
-  } else {
-    comm <- cmd.Process
-    cmd.Wait()
-    if shouldStop {
-      return
-    }
-    go waiter(comm, errs, stops, name, args...)
-  }
+	if err != nil {
+		errs <- err
+		return
+	} else {
+		comm <- cmd.Process
+		cmd.Wait()
+		if shouldStop {
+			return
+		}
+		go waiter(comm, errs, stops, name, args...)
+	}
 }
--- endless_runner/endless_runner_test.go
+++ endless_runner/endless_runner_test.go
@@ -1,103 +1,105 @@
 package endless_runner
 
 import (
-  "testing"
-  "os/exec"
-  "os"
-  "fmt"
-  "bufio"
-  "strings"
-  "strconv"
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"testing"
 )
 
 func handleError(err error) {
-  if err != nil {
-    fmt.Println(err)
-    os.Exit(1)
-  }
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
 
 func countProcs(target string) int {
-  ps := exec.Command("ps")
-  stdout, err := ps.StdoutPipe()
-  handleError(err)
+	ps := exec.Command("ps")
+	stdout, err := ps.StdoutPipe()
+	handleError(err)
 
-  err = ps.Start()
-  handleError(err)
+	err = ps.Start()
+	handleError(err)
 
-  defer ps.Wait()
+	defer ps.Wait()
 
-  buff := bufio.NewScanner(stdout)
-  var allText []string
+	buff := bufio.NewScanner(stdout)
+	var allText []string
 
-  for buff.Scan() {
-    allText = append(allText, buff.Text())
-  }
+	for buff.Scan() {
+		allText = append(allText, buff.Text())
+	}
 
-  matchedCount := 0
-  for _, item := range allText {
-    matched := strings.Contains(item, target)
-    if (matched) { matchedCount++ }
-  }
+	matchedCount := 0
+	for _, item := range allText {
+		matched := strings.Contains(item, target)
+		if matched {
+			matchedCount++
+		}
+	}
 
-  return matchedCount
+	return matchedCount
 }
 
 // That that Run actually runs the given process
 func TestRun(t *testing.T) {
-  // Run a meaningless process that never exits
-  procChan, _, stopChan := Run("tail", "-f", "/dev/null")
-  // Get the process out of the communication channel
-  process := <-procChan
-  specificCount := countProcs(strconv.Itoa(process.Pid))
-  if (specificCount != 1) {
-    t.Error("Reported pid was not found running by ps")
-  }
-  _, procErr := os.FindProcess(process.Pid)
-  if (procErr != nil) {
-    t.Error("Process not found")
-  }
-  stopChan <- true
+	// Run a meaningless process that never exits
+	procChan, _, stopChan := Run("tail", "-f", "/dev/null")
+	// Get the process out of the communication channel
+	process := <-procChan
+	specificCount := countProcs(strconv.Itoa(process.Pid))
+	if specificCount != 1 {
+		t.Error("Reported pid was not found running by ps")
+	}
+	_, procErr := os.FindProcess(process.Pid)
+	if procErr != nil {
+		t.Error("Process not found")
+	}
+	stopChan <- true
 }
 
 // See what happens when you pass an invalid command
 func TestFail(t *testing.T) {
-  procChan, errChan, _ := Run("asdasfafsa", "asdasf")
-  select {
-  case <-procChan:
-    t.Error("Shouldn't have returned anything on the proc channel")
-  case err := <-errChan:
-    if err.Error() != "exec: \"asdasfafsa\": executable file not found in $PATH" {
-      t.Error("Returned error doesn't look right")
-    }
-  }
+	procChan, errChan, _ := Run("asdasfafsa", "asdasf")
+	select {
+	case <-procChan:
+		t.Error("Shouldn't have returned anything on the proc channel")
+	case err := <-errChan:
+		if err.Error() != "exec: \"asdasfafsa\": executable file not found in $PATH" {
+			t.Error("Returned error doesn't look right")
+		}
+	}
 }
 
 // Ensure that a command is restarted when killed
 func TestDeath(t *testing.T) {
-  procChan, _, stopChan := Run("tail", "-f", "/dev/null")
-  process1 := <-procChan
+	procChan, _, stopChan := Run("tail", "-f", "/dev/null")
+	process1 := <-procChan
 
-  cmd := exec.Command("kill", strconv.Itoa(process1.Pid))
-  cmd.Start()
-  cmd.Wait()
+	cmd := exec.Command("kill", strconv.Itoa(process1.Pid))
+	cmd.Start()
+	cmd.Wait()
 
-  process2 := <-procChan
-  if process1.Pid == process2.Pid {
-    t.Error("Pids are not different")
-  }
+	process2 := <-procChan
+	if process1.Pid == process2.Pid {
+		t.Error("Pids are not different")
+	}
 
-  stopChan <- true
+	stopChan <- true
 }
 
 // Test that sending a stop stops the command
 func TestKill(t *testing.T) {
-  procChan, _, stopChan := Run("tail", "-f", "/dev/null")
-  process := <-procChan
+	procChan, _, stopChan := Run("tail", "-f", "/dev/null")
+	process := <-procChan
 
-  stopChan <- true
+	stopChan <- true
 
-  _ = <- stopChan
+	_ = <-stopChan
 
-  process.Wait()
+	process.Wait()
 }
