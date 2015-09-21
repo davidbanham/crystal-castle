40444eaa9ae17016e78ca0a7b9244bbef98b170d
David Banham <david@banham.id.au>
2015-09-21 21:02:16 +1000

    Compliant with prototype

--- destiny/destiny.go
+++ destiny/destiny.go
@@ -1,7 +1,12 @@
 package destiny
 
 import (
+	"crypto/md5"
+	"fmt"
+	"strconv"
 )
+
+const TARGET_HASHES = 2500
 
 type Job struct {
 	Job            string `json:"job"`
@@ -16,6 +21,12 @@ type Node struct {
 	Hostname string `json:"hostname"`
 }
 
+type Replicant struct {
+	Hostname       string `json:"hostname"`
+	HashAdjustment int    `json:"HashAdjustment"`
+	Hash           string `json:"Hash"`
+}
+
 type Manifest map[string][]Job
 
 type Plan map[string][]Job
@@ -25,6 +36,8 @@ type JobList []Job
 type AdjustedJobList []Job
 
 type NodeList []Node
+
+type ReplicatedNodeList []Replicant
 
 func BuildPlan(jobs JobList, nodes NodeList) (plan Plan) {
 	plan = make(Plan)
@@ -47,10 +60,91 @@ func BuildPlan(jobs JobList, nodes NodeList) (plan Plan) {
 	return plan
 }
 
-func AdjustJobs(jobs JobList, plan Plan) (ret AdjustedJobList) {
+func AdjustJobs(jobs JobList, plan Plan, nodes ReplicatedNodeList) (ret AdjustedJobList) {
+
+	adjusted := map[string]Job{}
+
+	for node, jobList := range plan {
+		for _, job := range jobList {
+			job = adjustJob(job, node, nodes)
+			adjusted[job.Job] = job
+		}
+	}
+
+	for _, job := range jobs {
+		ret = append(ret, adjusted[job.Job])
+	}
 	return ret
 }
 
-func BuildManifest(jobs AdjustedJobList, node NodeList) (manifest Manifest) {
+func adjustJob(job Job, target string, nodes ReplicatedNodeList) (ret Job) {
+	ret = job
+	winner := findMatchingNode(job, nodes)
+	if winner == target {
+		return ret
+	}
+	ret.HashAdjustment++
+	return adjustJob(ret, target, nodes)
+}
+
+func findMatchingNode(job Job, nodes ReplicatedNodeList) (winningNode string) {
+	highest := 0
+
+	for _, node := range nodes {
+		hash := hashString(node.Hash + job.Job + strconv.Itoa(job.HashAdjustment))
+
+		sum := HashValue(hash)
+
+		if sum > highest {
+			highest = sum
+			winningNode = node.Hostname
+		}
+	}
+	return winningNode
+}
+
+func HashValue(data string) (int) {
+	sum := 0
+	for _, r := range []rune(data) {
+		sum += int(r)
+	}
+	return sum
+}
+
+func BuildManifest(jobs AdjustedJobList, nodes NodeList, circle ReplicatedNodeList) (manifest Manifest) {
+	manifest = make(Manifest)
+
+	for _, node := range nodes {
+		manifest[node.Hostname] = []Job{}
+	}
+
+	for _, job := range jobs {
+		matchingNode := findMatchingNode(job, circle)
+		manifest[matchingNode] = append(manifest[matchingNode], job)
+	}
+
 	return manifest
+}
+
+func BuildReplicatedNodeList(nodes NodeList) (ret ReplicatedNodeList) {
+	for _, node := range nodes {
+		num_replicas := TARGET_HASHES / len(nodes)
+
+		i := num_replicas
+		for i > 0 {
+			newReplicant := Replicant{
+				Hostname: node.Hostname,
+				HashAdjustment: i,
+				Hash: hashString(node.Hostname + strconv.Itoa(i)),
+			}
+			ret = append(ret, newReplicant)
+			i--
+		}
+	}
+	return ret
+}
+
+func hashString(in string) (string) {
+	hash := md5.Sum([]byte(in))
+	return fmt.Sprintf("%x", hash)
 }
--- destiny/destiny_test.go
+++ destiny/destiny_test.go
@@ -14,6 +14,8 @@ var nodes NodeList
 var manifest, rebalanced Manifest
 var target Plan
 
+var adjusted AdjustedJobList
+
 func logErr(err error) {
 	if err != nil {
 		log.Fatal(err)
@@ -36,6 +38,9 @@ func TestMain(m *testing.M) {
 	_rebalanced, err := os.Open("fixtures/rebalanced.json")
 	logErr(err)
 
+	_adjusted, err := os.Open("fixtures/adjusted.json")
+	logErr(err)
+
 	jsonParser := json.NewDecoder(_jobs)
 	jsonParser.Decode(&jobs)
 
@@ -51,6 +56,9 @@ func TestMain(m *testing.M) {
 	jsonParser = json.NewDecoder(_rebalanced)
 	jsonParser.Decode(&rebalanced)
 
+	jsonParser = json.NewDecoder(_adjusted)
+	jsonParser.Decode(&adjusted)
+
 	retCode := m.Run()
 	os.Exit(retCode)
 }
@@ -62,11 +70,78 @@ func TestJson(t *testing.T) {
 	t.Log(manifest)
 	t.Log(target)
 	t.Log(rebalanced)
+	t.Log(adjusted)
 }
 
 func TestBuildPlan(t *testing.T) {
 	plan := BuildPlan(jobs, nodes)
 	equal := reflect.DeepEqual(plan, target)
+	if !equal {
+		t.Fail()
+	}
+}
+
+func TestBuildReplicatedNodeList(t *testing.T) {
+	replicated := BuildReplicatedNodeList(nodes)
+	numReplicants := len(replicated)
+	if (numReplicants != TARGET_HASHES) {
+		t.Fail()
+	}
+}
+
+func TestHashValue(t *testing.T) {
+	hash := "59bcc3ad6775562f845953cf01624225"
+	targetSum := 2059
+	result := HashValue(hash)
+	if (result != targetSum) { t.Fail() }
+}
+
+func TestAdjustJob(t *testing.T) {
+	testJob := Job{
+		Job: "Threatening Oryx",
+		HashAdjustment: 0,
+	}
+	replicatedNodes := BuildReplicatedNodeList(nodes)
+	adjusted := adjustJob(testJob, "one", replicatedNodes)
+	target := Job{
+		Job: "Threatening Oryx",
+		HashAdjustment: 4,
+	}
+	equal := reflect.DeepEqual(adjusted, target)
+	if !equal {
+		t.Fail()
+	}
+}
+
+func TestAdjustJobs(t *testing.T) {
+	plan := BuildPlan(jobs, nodes)
+	replicatedNodes := BuildReplicatedNodeList(nodes)
+	adjustedJobs := AdjustJobs(jobs, plan, replicatedNodes)
+	equal := reflect.DeepEqual(adjustedJobs, adjusted)
+	if !equal {
+		t.Fail()
+	}
+}
+
+func TestFindMatchingNode(t *testing.T) {
+	replicatedNodes := BuildReplicatedNodeList(nodes)
+	testJob := Job{
+		Job: "Threatening Oryx",
+		HashAdjustment: 4,
+	}
+	winner := findMatchingNode(testJob, replicatedNodes)
+	if winner != "one" {
+		t.Fail()
+	}
+}
+
+func TestBuildManifest(t *testing.T) {
+	plan := BuildPlan(jobs, nodes)
+	replicatedNodes := BuildReplicatedNodeList(nodes)
+	adjustedJobs := AdjustJobs(jobs, plan, replicatedNodes)
+
+	builtManifest := BuildManifest(adjustedJobs, nodes, replicatedNodes)
+	equal := reflect.DeepEqual(builtManifest, manifest)
 	if !equal {
 		t.Fail()
 	}
